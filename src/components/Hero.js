6d926ce554f03431603cb4d1031f1d442865a8d2
mustahsin2000 <mustahsin152415@gmail.com>
2023-04-14 00:31:36 +0600

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styles from '../style'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+  
+  <section id='home' className={`  ${styles.paddingY} 
+   top-[229px] left-[271px] right-[272px]  mx-[200px]
+  `}>
+     
+      <h1 className=' font-poppins font-semibold  text-[70px]
+      text-white text-center w-[897px] h-[306px]
+      '>
+      <span className='text-[#12BADF]'>MAKE</span> <span className='text-gradient3'>YOUR</span> <span className='text-gradient'>DREAM</span> <br />
+      <span className='text-[#12BADF]'>WEBSITE</span> <span className='text-gradient4'>STAND</span> <span className='text-gradient1'>OUT</span> <br />
+      <span className='text-[#12BADF]'>IN</span> <span className='text-[#17C7C0]'>A</span> <span className='text-gradient2'>CROWD</span>
+      </h1>
+      
+  </section>
+  
+)
 
 export default Hero
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,49 @@
 import React from 'react'
 import { useState } from 'react'
-import {close,logo,menu} from '../assets'
-import {navLinks} from '../constants'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 const Navbar = () => {
-  const [toggle,setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'> 
-    <div className='flex items-center'>
-    <img src={logo} alt="orbido"
-    className='w-[55px] h-[41px] top-[-111px] left-[4787px]'
-    />
-    <h4 className='text-white font-semibold text-lg w-[70px] h-[23px] top-[35px] left-[171.41px] font-poppins'>OR<span className='text-[#DE9A04]'>BIDO</span></h4>
-    </div>
-    <ul className='list-none sm:flex hidden justify-end items-center flex-1 w-[1135px] h-[23px]'>
-      {
-        navLinks.map((nav,index)=>(
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <div className='flex items-center'>
+        <img src={logo} alt="orbido"
+          className='w-[55px] h-[41px] top-[-111px] left-[4787px]'
+        />
+        <h4 className='text-white font-semibold text-lg w-[70px] h-[23px] top-[35px] left-[171.41px] font-poppins'>OR<span className='text-[#DE9A04]'>BIDO</span></h4>
+      </div>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1 w-[1135px] h-[23px]'>
+        {
+          navLinks.map((nav, index) => (
+            <li key={nav.id} className={`font-poppins font-normal cursor-pointer
           text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white w-[48px] h-[21px]
           `}>
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))
-      }
-    </ul>
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))
+        }
+      </ul>
 
-    <div className='sm:hidden flex flex-1 justify-end items-center'> 
-       <img src={toggle ? close : menu} alt="menu" 
-       onClick={()=>setToggle((prev)=>!prev)}
-       />
-       <div className={`${toggle ? 'flex':'hidden'} p-6 bg-blue-500 absolute
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={toggle ? close : menu} alt="menu"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-blue-500 absolute
        top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar
        `}>
- <ul className='list-none flex flex-col justify-end items-center flex-1'>
-      {
-        navLinks.map((nav,index)=>(
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {
+              navLinks.map((nav, index) => (
+                <li key={nav.id} className={`font-poppins font-normal cursor-pointer
           text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white w-[48px] h-[21px]
           `}>
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))
-      }
-    </ul>
-       </div>
-    </div>
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
